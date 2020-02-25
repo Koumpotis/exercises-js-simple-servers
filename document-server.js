dbf30fbb4ca57eb76f8e3a077ed5092512c2fb81
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-02-25 13:58:13 -0800

    all server exercises done

--- document-server.js
+++ document-server.js
@@ -1,22 +1,28 @@
 let net = require('net');
 let fs = require('fs');
 let serverLog = require('./lib/serverLog');
+let readlineSync = require('readline-sync');
 
-let SERVER_PORT = 2003;
+let SERVER_PORT = 2004;
 
 let server = net.createServer(function(connection) {
   let clientAddress = connection.remoteAddress;
 
-  serverLog('CONNECT', `Client at ${clientAddress} connected`);
-let message = fs.readFileSync('data/motd.txt', 'utf-8')
-  console.log(message);
-  connection.write(message);
-  connection.end();
+  connection.write('Select the file you want to see the contents of.\n');
+
+  connection.on('data', function(clientData) {
+    let fileSelection = String(clientData).trimRight();
+
+    let content = fs.readFileSync(`files/${fileSelection}`, 'utf-8');
+    console.log(content);
+    connection.write(content);
+    connection.end();
   /*
     1. Read the contents of data/motd.txt into memory
     2. Send the contents do the client using connection.write(...)
     3. Close the connection
   */
+  });
 });
 
 server.listen(SERVER_PORT, function() {
